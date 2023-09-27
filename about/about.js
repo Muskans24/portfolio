d00b2a7a2c3954b000c49c1aafd21313d2c6487a
Muskans24 <muskansingh2492003@gmail.com>
2023-09-27 12:33:42 +0530

    initial commit for portfolio

---
+++ about/about.js
@@ -0,0 +1,5 @@
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem =>{
+    var dots = elem.getAttribute('data-dots')
+    var marked = elem.getAttribute('data-percent')
+})
