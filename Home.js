1331c604be610d8d9866301ac76cbef8b3c9b6bb
Muskans24 <muskansingh2492003@gmail.com>
2023-10-23 15:21:37 +0530

    main heading change

---
+++ Home.js
@@ -0,0 +1,21 @@
+  // typing text animation script
+  
+  var typed = new Typed(".typing", {
+    strings: [
+      "Web Developer.",
+       "Freenser.",
+      "Web Designer.", ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+var typed = new Typed(".typing-2", {
+    strings: [
+      "Web Developer.",
+       "Freenser.",
+      "Web Designer.", ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
